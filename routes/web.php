abcae216c8b1c485070afcdd399189036682e360
Garvy63 <yogasetiawan022@gmail.com>
2025-11-27 18:41:18 +0700

    Fitur Login dan Regist (sudah connect database)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
+// <-- Login -->
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index.index');
+})->name('home')->middleware('auth');
+
+Route::get('/registration', function () {
+    return view('login.registration');
+})->name('registration');
+
+Route::post('/register', [RegistrationController::class, 'register'])
+->name('register');
+
+// Route untuk menampilkan form login
+Route::get('/login', [LoginController::class, 'showLoginForm'])
+->name('login');
+
+// Route untuk memproses data login (action dari form)
+Route::post('/login', [LoginController::class, 'login']);
+
+// Route untuk logout
+Route::post('/logout', [LoginController::class, 'logout'])
+->name('logout');
+
+// Route GET: Untuk menampilkan formulir
+Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])
+->name('register');
+
+// Route POST: Untuk memproses pengiriman data formulir
+Route::post('/register', [RegistrationController::class, 'register']);
